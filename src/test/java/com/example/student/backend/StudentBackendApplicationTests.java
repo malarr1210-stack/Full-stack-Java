52bbdafa2f5dae4294bf9f784d50575b159b13bf
malarr1210-stack <malarr1210@gmail.com>
2026-09-10 12:27:06 +0530

    Initial project

---
+++ src/test/java/com/example/student/backend/StudentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
